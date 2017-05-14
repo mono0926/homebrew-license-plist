81ec2fc82728e2cd610bbe66b6f48d5613fbbbd4
Masayuki Ono <mono0926@gmail.com>
2017-05-14 21:43:46 +0900

    Rename Formula

--- license-plist.rb
+++ license-plist.rb
@@ -1,4 +1,4 @@
-class Licenseplist < Formula
+class LicensePlist < Formula
   desc "License list generator for iOS applications."
   homepage "https://github.com/mono0926/LicensePlist"
   url "https://github.com/mono0926/LicensePlist/archive/1.3.2.tar.gz"
