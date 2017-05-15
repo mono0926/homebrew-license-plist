eba581ca534372e5f4591b1a4d0525af74a06a7f
Masayuki Ono <mono0926@gmail.com>
2017-05-15 18:46:25 +0900

    Update Formula

--- license-plist.rb
+++ license-plist.rb
@@ -1,6 +1,6 @@
 class LicensePlist < Formula
-  desc "License list generator for iOS applications."
-  homepage "https://github.com/mono0926/LicensePlist"
+  desc "A license list generator of all your dependencies for iOS applications"
+  homepage ""
   url "https://github.com/mono0926/LicensePlist/archive/1.3.5.tar.gz"
   sha256 "bae83138b45e062cf1cfa8ee627d7cf0912147e56a9bd93f643b63125d6dd182"
 
