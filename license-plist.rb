a7e4dbbcc8ebd7e7eed2637ba149a6b56b0afc7e
Masayuki Ono <mono0926@gmail.com>
2019-12-29 08:56:43 +0900

    Update version to 2.11.0

--- license-plist.rb
+++ license-plist.rb
@@ -1,8 +1,8 @@
 class LicensePlist < Formula
   desc "A license list generator of all your dependencies for iOS applications"
   homepage ""
-  url "https://github.com/mono0926/LicensePlist/archive/2.10.0.tar.gz"
-  sha256 "29dd96784f624c0a957bc6015d2d9980dd08813298c9ed3e13aed2141afa2d47"
+  url "https://github.com/mono0926/LicensePlist/archive/2.11.0.tar.gz"
+  sha256 "763d7de5720fd8d752a49f4b919943f45bd15d368d84f2da05c7319f98f054a1"
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
