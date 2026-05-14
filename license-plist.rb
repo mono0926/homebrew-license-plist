466238340dc38cdd6806cb008cb4a2bf989ca346
mono — Masayuki Ono <mono@mono0926.com>
2026-05-14 11:26:52 +0900

    Update version to 3.27.8

--- license-plist.rb
+++ license-plist.rb
@@ -1,8 +1,8 @@
 class LicensePlist < Formula
   desc "A license list generator of all your dependencies for iOS applications"
   homepage ""
-  url "https://github.com/mono0926/LicensePlist/archive/3.27.7.tar.gz"
-  sha256 "133f0400707121d3e0d88dd63b22a63f0b430bd6ff87acc7e69104050e93aa6e"
+  url "https://github.com/mono0926/LicensePlist/archive/3.27.8.tar.gz"
+  sha256 "7256964f8c19f07b75349bc8022c5ba0e1e4172bf5a38e2ecd259a95f1447aa8"
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
